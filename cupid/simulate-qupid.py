b54203a4999b58e79803aeca2707ae100592c76f
novarizark <lzhenn@mail2.sysu.edu.cn>
2019-10-13 10:55:22 +0800

    refresh

--- cupid/simulate-qupid.py
+++ cupid/simulate-qupid.py
@@ -5,42 +5,78 @@ matplotlib.use('Agg')
 
 class _sapiens:
     
-    def __init__(self, gender):
-        self.gender=gender
+    def __init__(self, id0):
         self.age=20
+        self.id0 = id0
         self.single=True
+    def gen_expect(self, n_sample):
+        """
+            generate adam/eve's lower bound of expectated lover's rank
+        """
+        if self.gender=='adam':
+            self.exp_lowerbnd=np.random.randint(int(self.rank-0.05*n_sample),int(self.rank+0.2*n_sample))
+        else:
+            self.exp_lowerbnd=np.random.randint(int(self.rank-0.2*n_sample),int(self.rank+0.05*n_sample))
 
     def try_match(self):
-        pass
+        """
+            as the motivated one, now he/she will try to match the one in his/her networking circle
+        """
 
 class _adam(_sapiens):
     
-    def __init__(self):
-        _sapiens.__init__(self, 'adam')
+    def __init__(self, id0):
+        _sapiens.__init__(self, id0)
+        
+        self.gender='adam'
         self.attract=np.random.randn()
         self.motivite=np.random.rand()
 
 class _eve(_sapiens):
     
-    def __init__(self):
-        _sapiens.__init__(self, 'eve')
+    def __init__(self, id0):
+        _sapiens.__init__(self, id0)
         
+        self.gender='eve'
         self.attract=np.random.rand()
         self.motivite=np.random.rand()/2.0
 
 
+# set init paras
 
-adam_list=[]
-eve_list=[]
+N_ADAM=50
+N_EVE=50
 
-for i in range(0,10):
-    adam_list.append(_adam())
-    eve_list.append(_eve())
+# individual networking circle volumn
+N_NETWORK=10
 
-print('Adam Score:')
-for adam in adam_list:
-    print(adam.attract)
+adam_dic={}
+eve_dic={}
 
-print('Eve Score:')
-for eve in eve_list:
-    print(eve.attract)
+# init adam and eve
+for i in range(0,N_ADAM):
+    adam_dic[i]=_adam(i)
+for i in range(0,N_EVE):
+    eve_dic[i]=_eve(i)
+
+# sort adam and eve by their attractive values
+adam_sort_list=sorted(adam_dic.values(), key=lambda adam: adam.attract)
+eve_sort_list=sorted(eve_dic.values(), key=lambda eve: eve.attract)
+
+# allocate rank #; generate lower bound of each adam or eve's expected lover's rank
+ii=0
+for adam in adam_sort_list:
+    adam.rank=ii
+    adam.gen_expect(N_ADAM)
+    ii=ii+1
+
+ii=0
+for eve in eve_sort_list:
+    eve.rank=ii
+    eve.gen_expect(N_EVE)
+    print(eve.exp_lowerbnd)
+    ii=ii+1
+
+# try to match one round, adam is motivated!
+for adam in adam_dic.values():
+    adam.try_match(N_NETWORK)
